5d2c50019f78355fed9483fd736509c058b70d0f
duanqiaoyanyu <qq825388931@gmail.com>
2022-02-26 22:34:23 +0800

    feat: 208. 实现 Trie (前缀树)

--- Algorithm/src/test/java/com/cskaoyan/Problem208.java
+++ Algorithm/src/test/java/com/cskaoyan/Problem208.java
@@ -15,13 +15,15 @@ public class Problem208 {
      * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
      * 输出
      * [null, null, true, false, true, null, true]
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
     class Trie {
+        // count 表示以当前单词结尾的单词数量
+        int count = 0;
         private char value;
         private Trie[] children = new Trie[26];
 
@@ -32,25 +34,58 @@ public class Problem208 {
         public void insert(String word) {
             char[] chars = word.toCharArray();
             int length = chars.length;
+            Trie current = this;
+            Trie child;
             for (int i = 0; i < length; i++) {
                 int index = chars[i] - 'a';
-                Trie child = children[index];
+                child = current.children[index];
                 if (child == null) {
-                    children[index].value = chars[i];
+                    child = new Trie();
+                    child.value = chars[i];
+                    current.children[index] = child;
                 }
 
-                continue;
+                current = child;
             }
+
+            current.count = 1;
         }
 
         public boolean search(String word) {
+            char[] chars = word.toCharArray();
+            int length = chars.length;
+            Trie current = this;
+            Trie child;
+            for (int i = 0; i < length; i++) {
+                int index = chars[i] - 'a';
+                child = current.children[index];
+                if (child == null) {
+                    return false;
+                }
 
+                current = child;
+            }
+
+            return current.count == 1;
         }
 
         public boolean startsWith(String prefix) {
+            char[] chars = prefix.toCharArray();
+            int length = chars.length;
+            Trie current = this;
+            Trie child;
+            for (int i = 0; i < length; i++) {
+                int index = chars[i] - 'a';
+                child = current.children[index];
+                if (child == null) {
+                    return false;
+                }
 
+                current = child;
+            }
+
+            return true;
         }
-    }
 
 /**
  *
@@ -60,4 +95,12 @@ public class Problem208 {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+
+// ------------------插入方法---------------
+// 1. 首先把 word 变成字符数组。前缀树的原理就是根节点不保存元素, 然后下面的孩子节点都保存一个字符
+// 然后从根节点开始的路径所组成的字符就是对应的字符串了。 所以一个相当于需要在根节点的孩子保存word的第一个字符,
+// 然后保存了第一个字符的节点的孩子保存了第二个字符。这里通过巧妙的设计把每一个节点的孩子们用一个大小为26的数组表示,
+// 下标0-25 分别代表着 'a'-'z'.然后如果那一层对应的元素没有就需要新增。直到将word数组遍历完成
+    }
 }
