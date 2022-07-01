079f32b23a992de4ea02d76e9c2e174b8944950c
duanqiaoyanyu <825388931@qq.com>
2022-07-01 19:36:41 +0800

    refactor: standardize maven project

---
+++ regex/src/main/java/com/cskaoyan/RegexApplication.java
@@ -0,0 +1,13 @@
+package com.cskaoyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegexApplication.class, args);
+    }
+
+}
---
+++ regex/src/test/java/com/cskaoyan/RegexApplicationTests.java
@@ -0,0 +1,9 @@
+package com.cskaoyan;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexApplicationTests {
+
+
+}
