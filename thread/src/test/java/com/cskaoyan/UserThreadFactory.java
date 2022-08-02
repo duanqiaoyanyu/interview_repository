1fb574f06c2665c1d185d7489c0f022c5004ffab
duanqiaoyanyu <825388931@qq.com>
2022-08-02 11:00:36 +0800

    refactor: refactor project structure

--- thread/src/test/java/com/cskaoyan/UserThreadFactory.java
+++ thread/src/test/java/com/cskaoyan/UserThreadFactory.java
@@ -1,9 +1,6 @@
 package com.cskaoyan;
 
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
