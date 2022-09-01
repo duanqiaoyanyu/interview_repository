f2bf1d6fc4e8d7d54e2b4fd6f7d8500f3f7bf00c
duanqiaoyanyu <825388931@qq.com>
2022-09-01 20:01:54 +0800

    feat: shardingsphere-jdbc

---
+++ shardingsphere-jdbc/src/main/java/com/cskaoyan/ShardingsphereJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.cskaoyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingsphereJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingsphereJdbcApplication.class, args);
+    }
+
+}
