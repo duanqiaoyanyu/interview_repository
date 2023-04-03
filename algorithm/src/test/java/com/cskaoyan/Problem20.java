ca4af7fafd3a3096bfbbfbb8feb6720cfbb0b39b
duanqiaoyanyu <825388931@qq.com>
2023-04-03 15:54:15 +0800

    feat: Problem20

---
+++ algorithm/src/test/java/com/cskaoyan/Problem20.java
@@ -0,0 +1,38 @@
+package com.cskaoyan;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author duanqiaoyanyu
+ * @date 2023/4/3 15:26
+ */
+public class Problem20 {
+
+    class Solution {
+        public boolean isValid(String s) {
+            char[] chars = s.toCharArray();
+            Deque<Character> deque = new ArrayDeque<>();
+
+            for (char aChar : chars) {
+                if (aChar == '[' || aChar == '{' || aChar == '(') {
+                    deque.push(aChar);
+                } else if (deque.isEmpty() || !isMatch(deque.pop(), aChar)) {
+                    return false;
+                }
+            }
+
+            return deque.isEmpty();
+        }
+
+        private boolean isMatch(Character left, char right) {
+            if (left == '(') {
+                return right == ')';
+            } else if (left == '{') {
+                return right == '}';
+            } else {
+                return right == ']';
+            }
+        }
+    }
+}
