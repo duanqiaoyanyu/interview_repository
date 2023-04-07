8a4658b14311b93db8b52591dcd68a686b0dffbe
duanqiaoyanyu <825388931@qq.com>
2023-04-07 22:10:07 +0800

    feat: leetcode

---
+++ algorithm/src/test/java/com/cskaoyan/Problem338.java
@@ -0,0 +1,30 @@
+package com.cskaoyan;
+
+public class Problem338 {
+
+    class Solution {
+        public int[] countBits(int n) {
+            int[] result = new int[n + 1];
+
+            for (int i = 0; i <= n; i++) {
+                result[i] = extract1Count(i);
+            }
+
+            return result;
+        }
+
+        private int extract1Count(int n) {
+            int count = 0;
+
+            while (n != 0) {
+                int remainder = n % 2;
+                if (remainder == 1) {
+                    count++;
+                }
+                n /= 2;
+            }
+
+            return count;
+        }
+    }
+}
