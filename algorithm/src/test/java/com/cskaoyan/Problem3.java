466c523bcbef4d4fb2f353a065e0ac2f046f3dc0
duanqiaoyanyu <825388931@qq.com>
2023-04-10 18:42:00 +0800

    feat: leetcode

---
+++ algorithm/src/test/java/com/cskaoyan/Problem3.java
@@ -0,0 +1,37 @@
+package com.cskaoyan;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author duanqiaoyanyu
+ * @date 2023/4/10 18:29
+ */
+public class Problem3 {
+
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s == null || s.length() == 0) {
+                return 0;
+            }
+
+            int result = 0;
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                Map<Character, Boolean> map = new HashMap<>();
+                int j;
+                for (j = i; j < chars.length; j++) {
+                    if (map.containsKey(chars[j])) {
+                        break;
+                    }
+
+                    map.put(chars[j], true);
+                }
+
+                result = Math.max(result, j - i);
+            }
+
+            return result;
+        }
+    }
+}
